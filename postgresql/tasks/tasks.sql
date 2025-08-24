a5b700bb29c73c9bda6b66f944fabb8e2924c6ea
TheEngineer <110096973+TheArchitect24@users.noreply.github.com>
2025-08-24 15:57:07 +0100

    Update tasks.sql

--- postgresql/tasks/tasks.sql
+++ postgresql/tasks/tasks.sql
@@ -1,1 +1,10 @@
+SHOW max_connections;
 
+CREATE USER backup_operator WITH PASSWORD 'password';
+CREATE ROLE backup;
+GRANT CONNECT ON DATABASE tolldata TO backup;
+USE tolldata;
+GRANT SELECT ON ALL TABLES IN SCHEMA toll TO backup;
+GRANT backup TO backup_operator;
+
+--use pgadmin4 on Windows client desktop
